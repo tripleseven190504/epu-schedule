d3286156319f8b99aa38e8fa7fb1cef01607ae96
Tạ Tuấn Anh <69012511+tripleseven190504@users.noreply.github.com>
2024-01-17 19:37:01 +0700

    Update epu_schedule.py

--- epu_schedule.py
+++ epu_schedule.py
@@ -14,7 +14,7 @@ from selenium.webdriver.common.by import By
 def initialize_driver():
     options = Options()
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-extensions")
     driver = webdriver.Chrome(options=options)
@@ -35,6 +35,7 @@ def process_captcha(driver, session):
         By.ID, 'imgSecurityCode').get_attribute('src')
     with open('captcha.png', 'wb') as file:
         file.write(session.get(captcha_image).content)
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     captcha_text = pytesseract.image_to_string(Image.open(
         'captcha.png'), lang='eng', config='--psm 6')
     os.remove('captcha.png')
@@ -54,27 +55,31 @@ def navigate_to_schedule_page(driver):
 def main(mssv, password):
     try:
         driver = initialize_driver()
-        driver.get("https://sinhvien.epu.edu.vn/Default.aspx")
-        cookies = {cookie['name']: cookie['value']
-                   for cookie in driver.get_cookies()}
-        user_agent = driver.execute_script("return navigator.userAgent;")
-        session = requests.Session()
-        session.headers.update({"User-Agent": user_agent})
-        session.cookies.update(cookies)
-        captcha_text = process_captcha(driver, session)
-        driver.find_element(
-            By.NAME, "ctl00$ucRight1$txtMaSV").send_keys(mssv)
-        driver.find_element(
-            By.NAME, "ctl00$ucRight1$txtMatKhau").send_keys(password)
-        driver.find_element(
-            By.NAME, "ctl00$ucRight1$txtSercurityCode").send_keys(captcha_text)
-        navigate_to_schedule_page(driver)
-        schedule_html_content = driver.find_element(
-            By.CLASS_NAME, 'div-ChiTietLich').get_attribute('innerHTML')
-        schedule_html_content = remove_inline_styles(schedule_html_content)
-        driver.quit()
-        return schedule_html_content
-    except UnexpectedAlertPresentException:
-        main(mssv, password)
+        try:
+            driver.get("https://sinhvien.epu.edu.vn/Default.aspx")
+            cookies = {cookie['name']: cookie['value']
+                    for cookie in driver.get_cookies()}
+            user_agent = driver.execute_script("return navigator.userAgent;")
+            session = requests.Session()
+            session.headers.update({"User-Agent": user_agent})
+            session.cookies.update(cookies)
+            captcha_text = process_captcha(driver, session)
+            driver.find_element(
+                By.NAME, "ctl00$ucRight1$txtMaSV").send_keys(mssv)
+            driver.find_element(
+                By.NAME, "ctl00$ucRight1$txtMatKhau").send_keys(password)
+            driver.find_element(
+                By.NAME, "ctl00$ucRight1$txtSercurityCode").send_keys(captcha_text)
+            navigate_to_schedule_page(driver)
+            schedule_html_content = driver.find_element(
+                By.CLASS_NAME, 'div-ChiTietLich').get_attribute('innerHTML')
+            schedule_html_content = remove_inline_styles(schedule_html_content)
+            driver.quit()
+            return schedule_html_content
+        except UnexpectedAlertPresentException:
+            driver.quit()
+            main(mssv, password)
+        except Exception as e:
+            return e
     except Exception as e:
         return e
