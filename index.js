b896db3880a8f8552f5225979b2314c67ea988be
Tạ Tuấn Anh <69012511+tripleseven190504@users.noreply.github.com>
2024-01-25 21:32:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function getScheduleHtmlContent(page) {
             <div id="noiDungDiv">
             ${scheduleHtmlContent}
             </div>
-            <div style="text-align: center;">
+            <div style="text-align: center; display: flex; justify-content: center;">
                 <button id="tuanNayButton" onclick="chuyenTrang('truoc')">Tuần này</button>
                 <button id="tuanSauButton" onclick="chuyenTrang('sau')">Tuần sau</button>
             </div>
