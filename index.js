249e089801f713865e8d11e3cd8e661defda1ea0
Tạ Tuấn Anh <69012511+tripleseven190504@users.noreply.github.com>
2024-01-26 12:27:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const axios = require('axios');
 const fs = require('fs');
 const FormData = require('form-data');
+const { get } = require('http');
 
 async function getScheduleHtmlContent(page) {
     const scheduleElement = await page.$('.div-ChiTietLich');
@@ -9,7 +10,7 @@ async function getScheduleHtmlContent(page) {
         return scheduleElement ? scheduleElement.innerHTML : null;
     }, scheduleElement);
     await page.click('#ctl00_ContentPlaceHolder_btnSau');
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(3000);
     const nextscheduleElement = await page.$('.div-ChiTietLich');
     const nextScheduleHtmlContent = await page.evaluate(nextscheduleElement => {
         return nextscheduleElement ? nextscheduleElement.innerHTML : null;
@@ -31,8 +32,13 @@ async function getScheduleHtmlContent(page) {
             <div class="container">
                 <button id="tuanNayButton" onclick="chuyenTrang('truoc')">Tuần này</button>
                 <button id="tuanSauButton" onclick="chuyenTrang('sau')">Tuần sau</button>
+                <button id="xemDiem">Xem điểm</button>
             </div>
-
+            <script>
+                document.getElementById("xemDiem").addEventListener("click", function() {
+                    window.location.href = "https://linhcute.shop/scores.html";
+                });
+            </script>
             <script>
                 var noiDungTuanNay = \`
                 ${scheduleHtmlContent}
@@ -49,6 +55,37 @@ async function getScheduleHtmlContent(page) {
                     }
                     document.getElementById("noiDungDiv").innerHTML = noiDungMoi;
                 }
+            </script>
+        </body>
+    </html>
+    `;
+    return finalHtmlContent;
+}
+async function getScoresHtmlContent(page) {
+    const scoresElement = await page.$('.tblKetQuaHocTap');
+    const scoresHtmlContent = await page.evaluate(scoresElement => {
+        return scoresElement ? scoresElement.innerHTML : null;
+    }, scoresElement);
+    const finalHtmlContent = `
+    <!DOCTYPE html>
+    <html lang="vi">
+        <head>
+            <meta charset="UTF-8">
+            <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tripleseven190504/epu-schedule@page/style.css"> -->
+            <link rel="stylesheet" href="./style.css">
+            <link rel="stylesheet" href="https://sinhvien.epu.edu.vn/css/mainpage/stylePhongDaotao.css">
+            <link rel="icon" href="https://raw.githubusercontent.com/tripleseven190504/epu-schedule/page/favicon.ico" type="image/x-icon">
+            <title>EPU Schedule</title>
+        </head>
+        <body>
+            <table class="grid grid-color2 tblKetQuaHocTap">
+            ${scoresHtmlContent.replace(/src="\.\.\//g, 'src="https://sinhvien.epu.edu.vn/')}
+            </table>
+            <button id="quayVe">Quay về trang chủ</button>
+            <script>
+                document.getElementById("quayVe").addEventListener("click", function() {
+                    window.location.href = "https://linhcute.shop/";
+                });
             </script>
         </body>
     </html>
@@ -90,9 +127,10 @@ const main = async () => {
         await fs.promises.writeFile('image.png', captchaImageBuffer.data);
         const captchaText = await processImage('./image.png', 'https://lens.google.com/v3/upload');
         await page.type('#ctl00_ucRight1_txtSercurityCode', captchaText);
-        await page.waitForTimeout(1000);
+        await page.waitForTimeout(1500);
         await page.keyboard.press('Enter');
-        await page.waitForNavigation();
+        // await page.waitForNavigation();
+        await page.waitForTimeout(3000);
         const targetUrl = 'https://sinhvien.epu.edu.vn/LichHocLichThiTuan.aspx';
         let retryCount = 1;
         while (page.url() !== targetUrl) {
@@ -105,7 +143,19 @@ const main = async () => {
         }
         console.log('Đang lấy dữ liệu...');
         const scheduleHtmlContent = await getScheduleHtmlContent(page);
+        const scoreUrl = 'https://sinhvien.epu.edu.vn/Xemdiem.aspx';
+        let retry = 1;
+        while (page.url() !== scoreUrl) {
+            await page.goto(scoreUrl);
+            retryCount += 1;
+            await page.waitForTimeout(1000);
+            if (retry > 5) {
+                break;
+            }
+        }
+        const scoresHtmlContent = await getScoresHtmlContent(page);
         await fs.promises.writeFile('index.html', scheduleHtmlContent);
+        await fs.promises.writeFile('scores.html', scoresHtmlContent);
         console.log('Lấy dữ liệu thành công!');
     } catch (error) {
         console.error('Lỗi không xác định');
