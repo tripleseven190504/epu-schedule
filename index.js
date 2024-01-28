946bac929a5f0fbb0bb5bbc56ae13825a2b306dd
tripleseven190504 <tripleseven190504@gmail.com>
2024-01-28 12:16:43 +0700

    faster

--- index.js
+++ index.js
@@ -2,95 +2,56 @@ const puppeteer = require('puppeteer');
 const axios = require('axios');
 const fs = require('fs');
 const FormData = require('form-data');
-const { get } = require('http');
 
 async function getScheduleHtmlContent(page) {
+    const targetUrl = 'https://sinhvien.epu.edu.vn/LichHocLichThiTuan.aspx';
+    let retryCount = 1;
+    while (page.url() !== targetUrl) {
+        await page.goto(targetUrl);
+        console.log("Đang lấy lịch học lần thứ ", retryCount);
+        retryCount += 1;
+        await page.waitForTimeout(1000);
+        if (retryCount > 5) {
+            break;
+        }
+    }
     const scheduleElement = await page.$('.div-ChiTietLich');
     const scheduleHtmlContent = await page.evaluate(scheduleElement => {
         return scheduleElement ? scheduleElement.innerHTML : null;
     }, scheduleElement);
+    return scheduleHtmlContent;
+}
+async function nextWeek(page) {
     await page.click('#ctl00_ContentPlaceHolder_btnSau');
     await page.waitForTimeout(3000);
     const nextscheduleElement = await page.$('.div-ChiTietLich');
     const nextScheduleHtmlContent = await page.evaluate(nextscheduleElement => {
         return nextscheduleElement ? nextscheduleElement.innerHTML : null;
     }, nextscheduleElement);
-    const finalHtmlContent = `
-    <!DOCTYPE html>
-    <html lang="vi">
-        <head>
-            <meta charset="UTF-8">
-            <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tripleseven190504/epu-schedule@page/style.css"> -->
-            <link rel="stylesheet" href="./style.css">
-            <link rel="icon" href="https://raw.githubusercontent.com/tripleseven190504/epu-schedule/page/favicon.ico" type="image/x-icon">
-            <title>EPU Schedule</title>
-        </head>
-        <body>
-            <div id="noiDungDiv">
-            ${scheduleHtmlContent}
-            </div>
-            <div class="container">
-                <button id="tuanNayButton" onclick="chuyenTrang('truoc')">Tuần này</button>
-                <button id="tuanSauButton" onclick="chuyenTrang('sau')">Tuần sau</button>
-                <button id="xemDiem">Xem điểm</button>
-            </div>
-            <script>
-                document.getElementById("xemDiem").addEventListener("click", function() {
-                    window.location.href = "https://linhcute.shop/scores";
-                });
-            </script>
-            <script>
-                var noiDungTuanNay = \`
-                ${scheduleHtmlContent}
-                \`;
-                var noiDungTuanSau = \`
-                ${nextScheduleHtmlContent}
-                \`;
-                function chuyenTrang(huong) {
-                    var noiDungMoi;
-                    if (huong === 'sau') {
-                        noiDungMoi = noiDungTuanSau;
-                    } else if (huong === 'truoc') {
-                        noiDungMoi = noiDungTuanNay;
-                    }
-                    document.getElementById("noiDungDiv").innerHTML = noiDungMoi;
-                }
-            </script>
-        </body>
-    </html>
-    `;
-    return finalHtmlContent;
+    return nextScheduleHtmlContent;
 }
 async function getScoresHtmlContent(page) {
+    const scoreUrl = 'https://sinhvien.epu.edu.vn/Xemdiem.aspx';
+    let retry = 1;
+    while (page.url() !== scoreUrl) {
+        await page.goto(scoreUrl);
+        console.log("Đang lấy điểm lần thứ ", retry);
+        retry += 1;
+        await page.waitForTimeout(1000);
+        if (retry > 5) {
+            break;
+        }
+    }
     const scoresElement = await page.$('.tblKetQuaHocTap');
     const scoresHtmlContent = await page.evaluate(scoresElement => {
         return scoresElement ? scoresElement.innerHTML : null;
     }, scoresElement);
-    const finalHtmlContent = `
-    <!DOCTYPE html>
-    <html lang="vi">
-        <head>
-            <meta charset="UTF-8">
-            <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tripleseven190504/epu-schedule@page/style.css"> -->
-            <link rel="stylesheet" href="./style.css">
-            <link rel="stylesheet" href="https://sinhvien.epu.edu.vn/css/mainpage/stylePhongDaotao.css">
-            <link rel="icon" href="https://raw.githubusercontent.com/tripleseven190504/epu-schedule/page/favicon.ico" type="image/x-icon">
-            <title>Điểm số</title>
-        </head>
-        <body>
-            <table class="grid grid-color2 tblKetQuaHocTap">
-            ${scoresHtmlContent.replace(/src="\.\.\//g, 'src="https://sinhvien.epu.edu.vn/')}
-            </table>
-            <button id="quayVe">Quay về trang chủ</button>
-            <script>
-                document.getElementById("quayVe").addEventListener("click", function() {
-                    window.location.href = "https://linhcute.shop/";
-                });
-            </script>
-        </body>
-    </html>
+    const finalScoreHtmlContent = `
+        <table class="grid grid-color2 tblKetQuaHocTap">
+        ${scoresHtmlContent.replace(/src="\.\.\//g, 'src="https://sinhvien.epu.edu.vn/')}
+        </table>
     `;
-    return finalHtmlContent;
+    return finalScoreHtmlContent;
 }
 const main = async () => {
     const browser = await puppeteer.launch({ headless: "new" });
@@ -135,33 +96,50 @@ const main = async () => {
         await page.waitForTimeout(1500);
         await page.keyboard.press('Enter');
         await page.waitForTimeout(3000);
-        const targetUrl = 'https://sinhvien.epu.edu.vn/LichHocLichThiTuan.aspx';
-        let retryCount = 1;
-        while (page.url() !== targetUrl) {
-            await page.goto(targetUrl);
-            console.log("Đang lấy lịch học lần thứ ", retryCount);
-            retryCount += 1;
-            await page.waitForTimeout(1000);
-            if (retryCount > 5) {
-                break;
-            }
-        }
         console.log('Đang lấy dữ liệu...');
         const scheduleHtmlContent = await getScheduleHtmlContent(page);
-        const scoreUrl = 'https://sinhvien.epu.edu.vn/Xemdiem.aspx';
-        let retry = 1;
-        while (page.url() !== scoreUrl) {
-            await page.goto(scoreUrl);
-            console.log("Đang lấy điểm lần thứ ", retry);
-            retry += 1;
-            await page.waitForTimeout(1000);
-            if (retry > 5) {
-                break;
-            }
-        }
+        const nextScheduleHtmlContent = await nextWeek(page);
         const scoresHtmlContent = await getScoresHtmlContent(page);
-        await fs.promises.writeFile('index.html', scheduleHtmlContent);
-        await fs.promises.writeFile('scores.html', scoresHtmlContent);
+        const finalHtmlContent = `
+        <!DOCTYPE html>
+        <html lang="vi">
+        <head>
+            <meta charset="UTF-8">
+            <title>EPU Schedule</title>
+            <link rel="stylesheet" href="./style.css">
+            <link rel="stylesheet" href="https://sinhvien.epu.edu.vn/css/mainpage/stylePhongDaotao.css">
+            <link rel="icon" href="./favicon.ico" type="image/x-icon">
+        </head>
+        <body>
+        <div id="noiDungDiv">
+        ${scheduleHtmlContent}
+        </div>
+        <div class="container">
+            <button id="tuanNayButton" onclick="chuyenTrang('truoc')">Tuần này</button>
+            <button id="tuanSauButton" onclick="chuyenTrang('sau')">Tuần sau</button>
+            <button id="xemDiem" onclick="chuyenTrang('diem')">Xem điểm</button>
+        </div>
+        <script>
+            const chuyenTrang = (trang) => {
+                const noiDungDiv = document.getElementById('noiDungDiv');
+                if (trang === 'truoc') {
+                    noiDungDiv.innerHTML = \`
+                    ${scheduleHtmlContent}
+                    \`;
+                } else if (trang === 'sau') {
+                    noiDungDiv.innerHTML = \`
+                    ${nextScheduleHtmlContent}
+                    \`;
+                } else if (trang === 'diem') {
+                    noiDungDiv.innerHTML = \`
+                    ${scoresHtmlContent}
+                    \`;
+                }
+            }
+        </script>
+        </body>
+        `
+        await fs.promises.writeFile('index.html', finalHtmlContent);
         console.log('Hoàn tất!');
     } catch (error) {
         console.error('Lỗi không xác định');
